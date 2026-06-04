9fd726acba4f6a118c181a4e3b68725c827548a3
BenvolioDAT <92760165+BenvolioDAT@users.noreply.github.com>
2026-06-03 22:37:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -67,40 +67,6 @@ module.exports.loop = function () {
         spawnManager.runRoom(requestReport.roomName);
     }
 
-    /*
-     * The large block below is old experimental spawn code kept as a reference.
-     * Because it is inside a block comment, JavaScript ignores it completely.
-     */
-    
-    var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'Upgrader');
-    //console.log('Upgrader:' + upgrader.length);
-
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    //console.log('Harvesters: ' + harvesters.length);
-
-    if(harvesters.length < 4) {
-        var newName = utility_spawn.genCreepName('Harvester');
-        console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-            {memory: {role: 'harvester'}});
-    }
-    
-    else if(upgrader.length <4) {
-        var newName = utility_spawn.genCreepName('Upgrader');
-        console.log('Spawning new Upgrader: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-            {memory: {role: 'Upgrader'}});
-    }
-    
-    if(Game.spawns['Spawn1'].spawning) { 
-        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-        Game.spawns['Spawn1'].room.visual.text(
-            '🛠️' + spawningCreep.memory.role,
-            Game.spawns['Spawn1'].pos.x + 1, 
-            Game.spawns['Spawn1'].pos.y, 
-            {align: 'left', opacity: 0.8});
-    }
-*/
 
     /*
      * Game.creeps contains every living creep you own, keyed by creep name.
